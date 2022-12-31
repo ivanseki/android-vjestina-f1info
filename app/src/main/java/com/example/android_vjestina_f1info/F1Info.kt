cc67f4b928255f2024594f006fc96e1977b26cfe
ivanseki <ivan.chernobyl007@gmail.com>
2022-12-31 13:51:07 +0100

    Initial commit with clean app project

---
+++ app/src/main/java/com/example/android_vjestina_f1info/F1Info.kt
@@ -0,0 +1,13 @@
+package com.example.android_vjestina_f1info
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class F1Info : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d("F1 Info", "App started")
+    }
+}
